bf259556cd7f082130c9874d1fcfe2de79c58929
Solam Nyangiwe <solamnyangiwe@gmail.com>
2024-05-20 13:12:34 +0200

    clean

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,6 @@ class Project < ApplicationRecord
   has_many :patches , through: :tasks
   accepts_nested_attributes_for :tasks, 
     reject_if: :all_blank, allow_destroy: true
+
+    delegate :patches,  to: :tasks, prefix: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,6 @@
 Rails.application.routes.draw do
   root "projects#index"
   resources :projects
-  resources :tasks, only: [], param: :index do
-    member do
-      delete '(:id)' => "tasks#destroy", as: ""
-      post '/' => "tasks#create"
-    end
-  end
-
-  resources :patches, only: [], param: :index do
-    member do
-      delete '(:id)' => "patches#destroy", as: ""
-      post '/' => "patches#create"
-    end
-  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
